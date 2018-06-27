f4e044dc9c24d765ca671881556a1187212e6b28
Hiraku Okabe <okabee326@yahoo.co.jp>
2018-06-27 13:35:56 +0000

    a

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
+      session[:book_id] = nil
       redirect_to user_path(user.id)
     else
       flash[:danger] = 'ログインに失敗しました'
